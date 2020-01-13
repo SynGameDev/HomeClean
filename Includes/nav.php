5f5f53c21469b3321868d66857202d08db2a47a9
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-14 00:24:11 +1100

    WIP

---
+++ Includes/nav.php
@@ -0,0 +1,6 @@
+<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+    <a class='navbar-brand' href='/index.php'><img src='Images/Icon.png' width='30' height='30' class='d-inline-clock align-top' alt=""> Home Clean Solutions</a>
+    <button class='navbar-toggler' type='button' data-toggle="collapse" data-target='#mainNav' aria-controls='mainNav' aria-expanded='false' aria-labal='Toggle-Navigation'>
+        <span class='navbar-toggler-icon'></span>
+    </button>
+</nav>
--- index.php
+++ index.php
@@ -28,11 +28,14 @@
             </a>
         </span>
         <span class='float-right' style='padding:5px;'>
-        <a href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
-        <a href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
-        <a href='#insta' class='social-link'><i class='fab fa-instagram'></i>
+        <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
+        <a target="_blank" href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
+        <a targer="_blank" href='#insta' class='social-link'><i class='fab fa-instagram'></i>
         </span>
     </div>
+
+    <!-- Navigation -->
+    <?php include_once('Includes/Nav.php'); ?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
