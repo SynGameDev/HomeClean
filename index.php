6325231a32053db0ca952d4b809be13ad544d1da
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-14 02:12:56 +1100

    Completed Index Page

--- index.php
+++ index.php
@@ -21,18 +21,7 @@
     <title>LANDING | HOMECLEANSOLUTIONS</title>
   </head>
   <body>
-    <div class="top-header">
-        <span class='float-left' style='margin-left:25px;margin-top:3px;'>
-            <a href='tel:0418514231'>
-                <span id='callus'>CALL US FOR A QUOTE |</span> <span id='num'>0418 514 231</span>
-            </a>
-        </span>
-        <span class='float-right social' style='margin-right: 25px;margin-top:3px;'>
-        <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
-        <a target="_blank" href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
-        <a targer="_blank" href='#insta' class='social-link'><i class='fab fa-instagram'></i>
-        </span>
-    </div>
+      <?php include_once('Includes/top-header.php'); ?>
 
     <!-- Navigation -->
     <?php include_once('Includes/Nav.php'); ?>
