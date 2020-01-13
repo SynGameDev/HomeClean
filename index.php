d88b563914d47880d7e1b2955a369aed89405814
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-13 23:36:48 +1100

    WIP

--- index.php
+++ index.php
@@ -13,15 +13,25 @@
     <link rel="icon" href="/Images/Icon.png" />
 
     <!-- Stylesheets -->
-    <link rel='stylesheet' type='text/css' href='/Styles/Main.css' />
+    <link rel='stylesheet' href='Styles/Main.css' />
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
+    <script src="https://kit.fontawesome.com/709e063d76.js" crossorigin="anonymous"></script>
+    
     <title>LANDING | HOMECLEANSOLUTIONS</title>
   </head>
   <body>
     <div class="top-header">
-        <span class='float-left'>CALL US FOR A QUOTE</span>
+        <span class='float-left' style='padding:5px;'>
+            <a href='tel:0418514231'>
+                <span id='callus'>CALL US FOR A QUOTE |</span> <span id='num'>0418 514 231</span>
+            </a>
+        </span>
+        <span class='float-right' style='padding:5px;'>
+        <a href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
+        <a href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
+        <a href='#insta' class='social-link'><i class='fab fa-instagram-sqaure'></i>
+        </span>
     </div>
 
     <!-- Optional JavaScript -->
