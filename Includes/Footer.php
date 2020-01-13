83bf9a0eb1820ebb9ae1787bf21950e45a2dd57e
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-14 02:13:25 +1100

    Completed HomePage

---
+++ Includes/Footer.php
@@ -0,0 +1,12 @@
+<div class='footer'>
+    <div class='content'>
+        <p class='footer-text'>
+            Copyright 2020 HomeCleanSolutions | All Rights Reserved
+        </p>
+        <div class='links'>
+            <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='footer-social'><i class="fab fa-facebook-square"></i></a>
+            <a target="_blank" href='#twitter' class='footer-social'><i class='fab fa-twitter-square'></i></a>
+            <a targer="_blank" href='#insta' class='footer-social'><i class='fab fa-instagram'></i></a>
+        </div>
+    </div>
+</div>
