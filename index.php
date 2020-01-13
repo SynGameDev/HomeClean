a70dcee9488c847c31c86365fdb3963905e6fca9
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-14 01:15:25 +1100

    WIP

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
 
     <!-- ICON -->
     <link rel="icon" href="/Images/Icon.png" />
@@ -17,17 +17,17 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/709e063d76.js" crossorigin="anonymous"></script>
-    
+
     <title>LANDING | HOMECLEANSOLUTIONS</title>
   </head>
   <body>
     <div class="top-header">
-        <span class='float-left' style='padding:5px;'>
+        <span class='float-left' style='padding:3px;'>
             <a href='tel:0418514231'>
                 <span id='callus'>CALL US FOR A QUOTE |</span> <span id='num'>0418 514 231</span>
             </a>
         </span>
-        <span class='float-right'>
+        <span class='float-right social' style='margin-right: 25px;margin-top:3px;'>
         <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
         <a target="_blank" href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
         <a targer="_blank" href='#insta' class='social-link'><i class='fab fa-instagram'></i>
@@ -38,8 +38,26 @@
     <?php include_once('Includes/Nav.php'); ?>
 
     <!-- Jumbotron -->
-    <div class='jumbotron'>
-
+    <div class='jumbotron jumbotron-fluid'>
+        <div class='container'>
+            <div class='row'>
+                <div class='col-sm'>
+                    <p class='jumboText black'>Need a quote? Contact Us Below!</p>
+                    <hr class='my-4' />
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-sm'>
+                    <p class='jumboText red'>Phone: <a href='tel:0418514231'>0418 514 231</a></p>
+                    <hr class='my-4' />
+                </div>
+            </div>
+            <div class='row'>
+                <div class='col-sm'>
+                    <p class='jumboText blue'>Email: <a href='email:homecleansolutions71@gmail.com'>homecleansolutions71@gmail.com</a></p>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Optional JavaScript -->
