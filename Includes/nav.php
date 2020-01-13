ea4ef7a7909607699dcadbdc609ab4fd05245f76
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-14 00:31:35 +1100

    WIP

--- Includes/nav.php
+++ Includes/nav.php
@@ -1,6 +1,28 @@
-<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class='navbar-brand' href='/index.php'><img src='Images/Icon.png' width='30' height='30' class='d-inline-clock align-top' alt=""> Home Clean Solutions</a>
     <button class='navbar-toggler' type='button' data-toggle="collapse" data-target='#mainNav' aria-controls='mainNav' aria-expanded='false' aria-labal='Toggle-Navigation'>
         <span class='navbar-toggler-icon'></span>
     </button>
+    <div class='collapse navbar-collapse' id='mainNav'>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a class="nav-link" href='/index.php'>Home</a>
+            </li>
+        </ul>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a class='nav-link' href='/AboutUs.php'>About Us</a>
+            </li>
+        </ul>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a class='nav-link' href='/Quote.php'>Request A Quote</a>
+            </li>
+        </ul>
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a class='nav-link' href='/ContactUs.php'>Contact Us / Leave a Review</a>
+            </li>
+        </ul>
+    </div>
 </nav>
--- index.php
+++ index.php
@@ -27,7 +27,7 @@
                 <span id='callus'>CALL US FOR A QUOTE |</span> <span id='num'>0418 514 231</span>
             </a>
         </span>
-        <span class='float-right' style='padding:5px;'>
+        <span class='float-right'>
         <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
         <a target="_blank" href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i>
         <a targer="_blank" href='#insta' class='social-link'><i class='fab fa-instagram'></i>
