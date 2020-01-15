d9b54b3ff526bff816961cd300abdb7e05fec148
Syndicategamedev <Syndicategamedev@gmail.com>
2020-01-15 18:26:59 +1100

    WIP

--- Includes/config.php
+++ Includes/config.php
@@ -1,5 +1,10 @@
 <?php
 
+$dbcon = new mysqli('localhost', 'u577142979_HCS_DB_Admin', 'MotherDuck@70', 'u577142979_HomeCleanSolut');
 
+if($dbcon->connect_error)
+{
+    die($dbconn->connect_error);
+}
 
 ?>
