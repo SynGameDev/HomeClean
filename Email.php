e8759aedc7b7efd784845c24cf459b7729f897db
Keegan Miller <Syndicategamedev@gmail.com>
2020-01-18 13:37:01 +1100

    WIP

---
+++ Email.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+
+</head>
+<style>
+
+body
+{
+    background-color:black;
+}
+
+.lean {
+    font-family: cursive;
+}
+
+</style>
+<body>
+    <center>
+Hi Jennifer,
+
+Thanks for your enquiry, someone from our team will be in touch shortly.
+
+<p class='lean'>"Hi! I'd love some tips on cleaning, can you please reply with some! thanks"</p>
+
+If you have any other question please contact us, or visit our website
+
+<a href="wip.homecleansolutions.com.au"><img src="homecleansolutions.com.au/Icon.png" /></a>
+
+Thanks,
+HomeClean Solutions.
+<span><a href="emailto:homecleansolutions71@gmail.com">Email Us</a> | <a href="phone:0418514231">Call Us</a> |
+</center>
+</body>
+
+</html>
