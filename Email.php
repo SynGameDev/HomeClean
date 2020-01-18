0a8344620d313137fa12e4758bde55c9d3d3e40f
Keegan Miller <Syndicategamedev@gmail.com>
2020-01-18 13:47:33 +1100

    WIP

--- Email.php
+++ Email.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-
+<script src="https://kit.fontawesome.com/709e063d76.js" crossorigin="anonymous"></script>
 </head>
 <style>
 
 body
 {
-    background-color:black;
+    background-color: darkgray;
     color: white;
 }
 
@@ -21,7 +21,22 @@ body
 </style>
 <body>
     <div class='wrapper'>
+        <h1 style='text-align:center'>-- Query Type --</h1>
+        <p class='email content'>
+            Hi -- NAME --, <br /> <br />
 
+            Thanks for your enquiry. we have received it and will be in touch shortly.
+
+            <br /> <br />
+            <span stle="font-family: cursive;">-- This is the customer enquiry --</span>
+
+            <br /><br />
+
+            Thanks,
+            HomeClean Solutions<br />
+            <a target="_blank" href='https://www.facebook.com/Homecleansolutions-100603651300911/?__tn__=%2Cd%2CP-R&eid=ARC0Zw5FwjBYgfuOe06HSLGdb2FWNflpBaLnxqA3yUsda7zrzLdltT8qPK-tbkHgRWiV0_YRTj2H_k92' class='social-link'><i class="fab fa-facebook-square"></i></a>
+            <a target="_blank" href='#twitter' class='social-link'><i class='fab fa-twitter-square'></i></a>
+            <a targer="_blank" href='#insta' class='social-link'><i class='fab fa-instagram'></i></a>
     </div>
 </body>
 
