3ac9ed8ee2608a7731831a161dc502a891b57740
Keegan Miller <Syndicategamedev@gmail.com>
2020-01-18 13:39:11 +1100

    WIP

--- Email.php
+++ Email.php
@@ -7,28 +7,36 @@
 body
 {
     background-color:black;
+    color: white;
 }
 
 .lean {
     font-family: cursive;
 }
 
+.content {
+    background-color: whitesmoke;
+}
+
 </style>
 <body>
-    <center>
-Hi Jennifer,
+<center>
+    <p class='content'>
+        Hi Jennifer,
+        <br />
+        Thanks for your enquiry, someone from our team will be in touch shortly.
+        <br />
 
-Thanks for your enquiry, someone from our team will be in touch shortly.
+        <p class='lean'>"Hi! I'd love some tips on cleaning, can you please reply with some! thanks"</p><br />
 
-<p class='lean'>"Hi! I'd love some tips on cleaning, can you please reply with some! thanks"</p>
+        If you have any other question please contact us, or visit our website <br />
 
-If you have any other question please contact us, or visit our website
+        <a href="wip.homecleansolutions.com.au"><img src="homecleansolutions.com.au/Icon.png" /></a><br />
 
-<a href="wip.homecleansolutions.com.au"><img src="homecleansolutions.com.au/Icon.png" /></a>
-
-Thanks,
-HomeClean Solutions.
-<span><a href="emailto:homecleansolutions71@gmail.com">Email Us</a> | <a href="phone:0418514231">Call Us</a> |
+        Thanks,<br />
+        HomeClean Solutions.<br />
+        <span><a href="emailto:homecleansolutions71@gmail.com">Email Us</a> | <a href="phone:0418514231">Call Us</a> |
+</p>
 </center>
 </body>
 
